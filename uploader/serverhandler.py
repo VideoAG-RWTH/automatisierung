ceeda2963d682557c55df990e7efcea2d353078f
Moritz Holtz <moritz.holtz@rwth-aachen.de>
2014-09-18 15:41:18 +0200

    interface for tests, support for human paths
    Tests are being defined in tests.py and must be listed in the array alltests.
    The variable NAME in the test class contains the name of the test.
    They return a dictionary {subtestname: (good, data),...} where good is boolean and data is a string.
    Also, they must be defined in the database in the tables tests, subtest and testending.
    
    By using
    ./tools.py server.conf link <fileids>
    you can now link the files to the human dir, as defined in the server.conf

--- uploader/serverhandler.py
+++ uploader/serverhandler.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import time
 import comlib
 import hashlib
 import identify
 import filelib
-#import checker
+import tests
 
 def uphandle(socket, db, log, config):
     log.log(3, "starting indexhandle")
@@ -20,7 +18,7 @@ def uphandle(socket, db, log, config):
             return
     
     prop = db.getfileprop(id)
-    realname = config["datadir"] + "/" + filelib.getfilename(prop)
+    realname = config["dirconf"]["data"] + "/" + filelib.getstorename(prop)
     if prop["path"] != None:
         log.log(2, "id '" + str(id) + "' already present")
         realname = prop["path"]
@@ -33,14 +31,20 @@ def uphandle(socket, db, log, config):
     log.log(2, "uploading id '" + str(id) + "' as file '" + realname + "'")
     
     size = prop["size"]
-    mtime = prop["mtime"]
     
     comlib.sendcom(socket,{"status":"ok"})
     log.log(3, "receiving " + str(size) + " bytes")
     
     fobj = open(realname,"wb")
     hasher = hashlib.md5()
-    #cmts = checker.MP2T()
+    
+    ending = prop["filename"].split(".")[-1].lower()
+    availtest = db.posstest(ending)
+    testobjs = []
+    for test in tests.alltests:
+        if test.NAME in availtest:
+            testobjs.append(test())
+    
     read = 1
     while size > 0:
         if size < config["chunksize"]:
@@ -50,12 +54,15 @@ def uphandle(socket, db, log, config):
         data = socket.recv(toread)
         read = len(data)
         hasher.update(data)
-        #cmts.update(data)
+        for obj in testobjs:
+            obj.update(data)
         fobj.write(data)
         size-=read
     fobj.close()
-    os.utime(realname, times=(time.time(),float(mtime)))
     md5 = hasher.hexdigest()
+    for obj in testobjs:
+        res = obj.final()
+        db.indextest(id, obj.NAME, res)
     com = comlib.recvcom(socket)
     if com["md5"] != md5:
         comlib.sendcom(socket,{"status":"bad"})
--- uploader/tests.py
+++ uploader/tests.py
@@ -4,24 +4,28 @@
 import sys
 
 class MP2T(object):
+    NAME="mp2t"
     def __init__(self):
         self.left = 4
         self.read = 0
+        self.good = True
     
     def update(self, data):
         read = len(data)
         ret = read
-        while read > self.left:
-            read-=self.left
-            self.read+=self.left
-            if not data[-read] == 71: #b"G"[0]=71
-                raise Exception(self.read)
-            self.left = 192
-        self.left-=read
+        if self.good:
+            while read > self.left:
+                read-=self.left
+                self.read+=self.left
+                if not data[-read] == 71: #b"G"[0]=71
+#                    raise Exception(self.read)
+                    self.good=False
+                self.left = 192
+            self.left-=read
         return ret
             
     def final(self):
-        pass
+        return {"mp2t": (self.good,"")}
 
 if __name__ == "__main__":
     for i in range(2, len(sys.argv)):
@@ -37,3 +41,6 @@ if __name__ == "__main__":
             print(" passed")
         except Exception as err:
             print(" failed with " + str(format(err)))
+            
+
+alltests = [MP2T]
