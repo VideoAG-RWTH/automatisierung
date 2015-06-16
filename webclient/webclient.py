e2a9e1684d941758f32a6557f8d3ea0ae2078e7b
Moritz Holtz <moritz.holtz@rwth-aachen.de>
2015-06-16 13:09:27 +0200

    declared app in webclient.py

--- webclient/webclient.py
+++ webclient/webclient.py
@@ -8,6 +8,7 @@ import json
 
 import config
 
+app = Flask(__name__)
 app.config.from_object('config')
 
 def reqapi(token, request, data={}):
@@ -20,7 +21,7 @@ def reqapi(token, request, data={}):
 
 @app.route('/')
 def index():
-	events = reqapi("aab8d0dc34b59ee9e804f1261a2da9a82feafb64054cd40c4a62fa0767aed228096e558b13452623579208ed33904c6762c4ad625c4e7b25c2b2688e2e5ebb0c", getunindexed)
+	events = reqapi("aab8d0dc34b59ee9e804f1261a2da9a82feafb64054cd40c4a62fa0767aed228096e558b13452623579208ed33904c6762c4ad625c4e7b25c2b2688e2e5ebb0c", "getunindexed")
 	return render_template("index.html", events=events)
 
 if __name__ == '__main__':
